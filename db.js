260b6dcf327edcfc5258db2a910fe0139ec1eedc
casualshay <akshaysprabhakar@gmail.com>
2019-09-27 15:36:57 -0700

    fullstack app almost done

--- db.js
+++ db.js
@@ -51,28 +51,19 @@ const sync = async () => {
     { name: 'teach thunks (9/30)' },
     { name: 'assign solo project (10/2)' }
   ];
-  //const [ boston, la, slo ] = await Promise.all(place.map( pla => Place.create(pla)));
 
   const thomas = [
     { name: 'help akshay' },
     { name: 'make new ds' },
     { name: 'look for 300th job' }
   ];
-//   const [ moe, larry, curly ] = await Promise.all(person.map( per => Person.create(per)));
-//   await Promise.all([ moe.update({ placeId : boston.id }) ])
-//   await Promise.all([ larry.update({ placeId : la.id }) ])
-//   await Promise.all([ curly.update({ placeId : slo.id }) ])
 
   const matt = [
     { name: 'work in outdated languages' },
     { name: 'help everyone' },
     { name: 'find bugs' }
   ];
-//   const [ plant, pencil, stapler ] = await Promise.all(things.map( thi => Thing.create(thi)));
-//   await Promise.all([ plant.update({ personId : moe.id }) ])
-//   await Promise.all([ pencil.update({ personId : larry.id }) ])
-//   await Promise.all([ stapler.update({ personId : curly.id }) ])
-    
+   
 	const jonathan = [
     { name: 'help class' },
     { name: 'work with other cohort' },
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const db = require('./db');
+const { models: { Prof, Thomas, Matt, Jonathan } } = require('./db');
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/prof', async ( req, res, next ) => {
+  try {
+    res.send(await Prof.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
+
+app.get('/thomas', async ( req, res, next ) => {
+  try {
+    res.send(await Thomas.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
+
+app.get('/matt', async ( req, res, next ) => {
+  try {
+    res.send(await Matt.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
+
+app.get('/jonathan', async ( req, res, next ) => {
+  try {
+    res.send(await Jonathan.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
+
+db.sync()
+  .then(() => {
+      app.listen(port, () => console.log(`listening on port ${port}`));
+  })
